46352ff61abde1e0ce2b2b4485cb67c6d8ac7259
zhanqianli <zhanqianli@stbl.cc>
2019-01-06 15:20:48 +0800

    Initial Commit

---
+++ MSRequestFramework/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MSRequestFramework
+//
+//  Created by 詹前力 on 2019/1/6.
+//  Copyright © 2019年 Liven. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ MSRequestFrameworkTests/MSRequestFrameworkTests.m
@@ -0,0 +1,37 @@
+//
+//  MSRequestFrameworkTests.m
+//  MSRequestFrameworkTests
+//
+//  Created by 詹前力 on 2019/1/6.
+//  Copyright © 2019年 Liven. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface MSRequestFrameworkTests : XCTestCase
+
+@end
+
+@implementation MSRequestFrameworkTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
